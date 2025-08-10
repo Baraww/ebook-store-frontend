fb6325d91407324f11d46c96e362444e06a48e85
Abdulmumini1 <shaibuabdulmumini1@gmail.com>
2025-08-10 06:46:05 -0700

    FIX: Clean and correct ContactPage.js file

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -7,15 +7,19 @@ const ContactPage = () => {
     <div className="add-book-form">
       <h1>Contact Us</h1>
       <p>Have a question or comment? Fill out the form below and we'll get back to you.</p>
-      
+
       <form name="contact" method="POST" data-netlify="true" action="/thank-you.html">
         <input type="hidden" name="form-name" value="contact" />
+
         <label>Your Name:</label>
         <input type="text" name="name" required />
+
         <label>Your Email:</label>
         <input type="email" name="email" required />
+
         <label>Message:</label>
         <textarea name="message" required></textarea>
+
         <button type="submit">Send Message</button>
       </form>
     </div>
